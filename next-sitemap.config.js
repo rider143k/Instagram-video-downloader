a48fc448f16cb20f3b7411e1ee16cbe110080e51
Aniket Singh <ytaniketsingh@gmail.com>
2026-01-20 23:21:57 +0530

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   siteUrl: 'https://instad.pastex.online',
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
   transform: async (config, path) => {
     return {
       loc: path,
       changefreq: 'daily',
-      priority: 0.8,
+      priority: path === '/' ? 1.0 : 0.8,
       lastmod: new Date().toISOString(),
+      alternateRefs: config.alternateRefs ?? [],
     };
   },
 };
